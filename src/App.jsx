cf6c9307bda2c09e1a27419f3ab5e7cbbcc77d0e
sungyh52 <sungyh5252@gmail.com>
2023-04-25 10:59:29 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Main from "./pages/main";
+import Detail from "./pages/detail";
+import Header from "./components/Header";
+
 function App() {
-  return <div className="bg-red-100">Hello, React</div>;
+  return (
+    <BrowserRouter>
+      <div className="bg-red-100">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/:tokenId" element={<Detail />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
