ee9c81cc0f07e500c66774442b4848032b28c326
sungyh52 <sungyh5252@gmail.com>
2023-04-25 11:49:34 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Main from "./pages/main";
 import Detail from "./pages/detail";
 import Header from "./components/Header";
+import {useState} from "react";
 
 function App() {
+  const [account, setAccount] = useState("");
+
   return (
     <BrowserRouter>
-      <div className="bg-red-100">
-        <Header />
+      <div className="min-h-screen bg-gray-950 text-white">
+        <Header account={account} setAccount={setAccount} />
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/:tokenId" element={<Detail />} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,21 @@
 import {FaChessRook} from "react-icons/fa";
 import {BiWallet} from "react-icons/bi";
+import {AiFillHeart} from "react-icons/ai";
 import {Link} from "react-router-dom";
 
-const Header = () => {
+const Header = ({account, setAccount}) => {
+  const onClickAccount = async () => {
+    try {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      setAccount(accounts[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <header className="max-w-screen-xl mx-auto p-4 flex justify-between items-center font-bold">
       <Link to="/">
@@ -12,12 +25,27 @@ const Header = () => {
         </div>
       </Link>
       <div>
-        <button className="flex items-center p-2 bg-gray-800 rounded-full">
-          <div className="bg-main w-6 h-6 rounded-full flex justify-center items-center">
-            <BiWallet />
+        {account ? (
+          <div className="flex items-center p-2 bg-gray-800 rounded-full">
+            <div className="bg-main w-6 h-6 rounded-full flex justify-center items-center">
+              <AiFillHeart />
+            </div>
+            <div className="ml-1">
+              {account.substring(0, 4)}...
+              {account.substring(account.length - 4)}
+            </div>
           </div>
-          <div className="ml-1">Connect</div>
-        </button>
+        ) : (
+          <button
+            className="flex items-center p-2 bg-gray-800 rounded-full"
+            onClick={onClickAccount}
+          >
+            <div className="bg-main w-6 h-6 rounded-full flex justify-center items-center">
+              <BiWallet />
+            </div>
+            <div className="ml-1">Connect</div>
+          </button>
+        )}
       </div>
     </header>
   );
