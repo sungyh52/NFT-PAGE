1566a19387fc2e93c2ac06e24bae40f1421ef79d
sungyh52 <sungyh5252@gmail.com>
2023-04-25 11:33:21 +0900

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,26 @@
+import {FaChessRook} from "react-icons/fa";
+import {BiWallet} from "react-icons/bi";
+import {Link} from "react-router-dom";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="max-w-screen-xl mx-auto p-4 flex justify-between items-center font-bold">
+      <Link to="/">
+        <div className="flex items-center text-main">
+          <FaChessRook size={28} />
+          <div className="ml-1 text-xl">Ble-Chess</div>
+        </div>
+      </Link>
+      <div>
+        <button className="flex items-center p-2 bg-gray-800 rounded-full">
+          <div className="bg-main w-6 h-6 rounded-full flex justify-center items-center">
+            <BiWallet />
+          </div>
+          <div className="ml-1">Connect</div>
+        </button>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
